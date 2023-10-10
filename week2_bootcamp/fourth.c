882456fdb76c91255b613aea39cd3c2bce3825f2
AliDib38 <146854566+AliDib38@users.noreply.github.com>
2023-10-10 16:08:46 +0000

    week2

---
+++ week2_bootcamp/fourth.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    float a;
+    float b;
+    printf("What is the height of the rectangle? \n");
+    scanf("%f", &a);
+    printf("what is the width of the rectangle? \n");
+    scanf("%f", &b);
+    printf();
+
+}
